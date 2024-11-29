1e43e233fb18e343f2586bc9e4129970e3d9691b
Ankita Kumari <khushisitamarhi@gmail.com>
2024-11-29 16:08:33 +0530

    starte-code

---
+++ routes/listing.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+
+const lisitngController = require("../controllers/listings.js");
+
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+router.route("/")
+    .get(wrapAsync(lisitngController.index))
+    .post(isLoggedIn, upload.single("listing[image]"), validateListing, wrapAsync(lisitngController.createListing));
+
+//New Route
+router.get("/new", isLoggedIn, lisitngController.renderNewForm);
+
+router.route("/:id")
+    .get(wrapAsync(lisitngController.showListing))
+    .put(isLoggedIn, isOwner, upload.single("listing[image]"), validateListing, wrapAsync(lisitngController.updateListing))
+    .delete(isLoggedIn, isOwner, wrapAsync(lisitngController.destroyListing));
+
+//Edit Route
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(lisitngController.renderEditForm));
+
+module.exports = router;
